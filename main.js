98f6a7025c5814133ff4977983c82e7026003a02
Saloni <saloniswagata@gmail.com>
2020-08-07 22:34:31 +0530

    min-max errors

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ const api = {
     weather_el.innerText = weather.weather[0].main;
   
     let hilow = document.querySelector('.hi-low');
-    hilow.innerText = '${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c';
+    hilow.innerText = ${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c;
   }
   
   function dateBuilder (d) {
